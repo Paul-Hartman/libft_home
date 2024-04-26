33ea4a1fb14291f3c52084da7a6e2234531b4411
Paul Hartman <phartman@student.42berlin.de>
2024-04-26 16:14:32 +0200

    added null protection

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: phartman <phartman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 14:50:05 by phartman          #+#    #+#             */
-/*   Updated: 2024/04/26 14:34:36 by phartman         ###   ########.fr       */
+/*   Updated: 2024/04/26 14:40:52 by phartman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,4 +30,3 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	}
 	return (dest);
 }
-
